9f3a8618d1bf87507fc1d47eb0361d2784eaecc5
dev <dev@mindtree.com>
2020-04-06 20:35:17 +0530

    search

---
+++ searchManagementService/src/test/java/com/searchManagementService/SearchManagementServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.searchManagementService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SearchManagementServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
